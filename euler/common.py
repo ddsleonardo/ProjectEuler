77fe666f3b2766ba0caeb2be1288094d683cf4fe
BARF1\ldonisetedasilva <ldonisetedasilva@mercedesamgf1.com>
2019-10-14 18:46:13 +0100

    Implemented an initial primality test function

--- euler/common.py
+++ euler/common.py
@@ -1,9 +1,20 @@
 import math
 
-def digit_count(n: int):
-    count = 1
-    while (n >= 10):
-        n = n // 10
-        count = count + 1
+def is_prime(n: int):
+    ''' For a given integer n, returns true if
+        n is a prime number '''
 
-    return count
+    if (n < 4):
+        return n > 1
+
+    if (n % 2 == 0 or n % 3 == 0):
+        return False
+
+    div = 5
+
+    while (div * div <= n):
+        if (n % div == 0 or n % (div + 2) == 0):
+            return False
+        div += 6
+
+    return True
