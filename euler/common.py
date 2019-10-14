9fa186734f73f8dba24ac8c1b6e4a5a87ea51ef7
BARF1\ldonisetedasilva <ldonisetedasilva@mercedesamgf1.com>
2019-10-14 17:58:31 +0100

    Initiated euler as a package. Simplified digit_count

--- euler/common.py
+++ euler/common.py
@@ -2,7 +2,8 @@ import math
 
 def digit_count(n: int):
     count = 1
-    while (n >= math.pow(10, count)):
+    while (n >= 10):
+        n = n // 10
         count = count + 1
 
     return count
