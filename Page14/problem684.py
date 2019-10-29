694b5ecc77d4f9e495b91c8452c791ee09050929
BARF1\ldonisetedasilva <ldonisetedasilva@mercedesamgf1.com>
2019-10-29 18:58:50 +0000

    Updated solutions to use the euler common module

--- Page14/problem684.py
+++ Page14/problem684.py
@@ -5,33 +5,6 @@ sys.path.append(r'..\euler')
 import common as euler
 import time
 
-def large_power_mod(base: int, exponent: int, mod: int):
-
-    if (base == 0):
-        return 0
-    if (exponent == 0):
-        return 1
-
-    # First of all try to reduce the base
-    base %= mod
-    res = 1
-
-    # Logic: base^exponent = product of base^(powers of 2),
-    # looping over all powers of 2 you'd need to add in order to get exponent
-    while (exponent > 0):
-        exp_2 = 1
-        base_power_power_n = base
-        while (2 ** exp_2 <= exponent):
-            base_power_power_n *= base_power_power_n
-            base_power_power_n %= mod
-            exp_2 += 1
-
-        res *= base_power_power_n
-        res %= mod
-        exponent -= (2 ** (exp_2 - 1))
-
-    return res
-
 def return_smallest_digit_sum(n: int, mod: int):
     ''' Returns the smallest integer (mod n)
     whose digit sum equals n '''
@@ -41,7 +14,7 @@ def return_smallest_digit_sum(n: int, mod: int):
     # If r = 0 we get the first formula!
     k = n // 9
 
-    return ((n % 9 + 1) * (large_power_mod(10, k, mod) - 1) + (n % 9)) % mod
+    return ((n % 9 + 1) * (euler.large_power_mod(10, k, mod) - 1) + (n % 9)) % mod
 
 def return_sum_until_n(n: int, mod: int):
 
@@ -51,7 +24,7 @@ def return_sum_until_n(n: int, mod: int):
     # then calculate the rest one by one
     k = n // 9
     
-    return ((6 * (large_power_mod(10, k, mod) - 1) - 9 * k) % mod) + (sum([return_smallest_digit_sum(i, mod) for i in range(n - n % 9 + 1, n + 1)]) % mod)
+    return ((6 * (euler.large_power_mod(10, k, mod) - 1) - 9 * k) % mod) + (sum([return_smallest_digit_sum(i, mod) for i in range(n - n % 9 + 1, n + 1)]) % mod)
 
 def Problem684(n: int):
     ''' Returns the sum defined in the problem 
--- Page2/problem62.py
+++ Page2/problem62.py
@@ -5,43 +5,6 @@ sys.path.append(r'..\euler')
 import common as euler
 import time
 
-def sum_digits(n: int):
-    ''' Returns the sum of the digits of a given integer n'''
-
-    sum = 0
-    while n:
-        sum += n % 10
-        n //= 10
-
-    return sum
-
-def is_permutation(a: int, b: int):
-    ''' Returns true if the one of the two given integers
-    can be obtained by a permutation of the digits of the other'''
-
-    if (len(str(a)) != len(str(b))):
-        return False
-
-    # Try the sum first
-    if (sum_digits(a) != sum_digits(b)):
-        return False
-
-    str_a = str(a)
-    str_b = str(b)
-
-    digits_a = [str_a[i] for i in range(len(str_a))]
-    digits_b = [str_b[i] for i in range(len(str_b))]
-
-    # For each digit in a, we try to remove it from b
-    # If they are permutations of each other, this will never return an error
-    for digit in digits_a:
-        try:
-            i = digits_b.remove(digit)
-        except ValueError:
-            return False
-
-    return True
-
 def Problem62(n: int):
     ''' For a positive integer n, returns the smallest
     cube that has n digit permutations which are also cubes'''
@@ -68,7 +31,7 @@ def Problem62(n: int):
         found_permutation = False
         # First of all, let's try to find a permutation in the existing permutation groups
         for permutation in permutations:
-            if (is_permutation(new_cube, permutation[0])):
+            if (euler.is_permutation(new_cube, permutation[0])):
                 found_permutation = True
                 permutation.append(new_cube)
                 # If this permutation has now reached n items, we return the smallest (always the first) number
@@ -79,12 +42,12 @@ def Problem62(n: int):
         if (not found_permutation):
             # cubes is a dict where values are grouped by a key which is the digit sum
             # this is useful because all candidate permutations will necessary have equal digit sum
-            key = sum_digits(new_cube)
+            key = euler.sum_digits(new_cube)
             if (key in cubes):
                 for cube in cubes[key]:
                     # If we find a permutation, we insert the new pair in the permutations list
                     # We also remove the cube from the singles collection, for we don't need to check it again
-                    if (is_permutation(new_cube, cube)):
+                    if (euler.is_permutation(new_cube, cube)):
                         found_permutation = True
                         permutations.append([cube, new_cube])
                         cubes[key].remove(cube)
